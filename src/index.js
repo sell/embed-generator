c1a2c134c041f583db84fcd7166062af6b2b9c53
Jay <34258275+sell@users.noreply.github.com>
2020-11-02 19:12:16 -0500

    port issues:

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000
 
 const start = async () => {
   try {
-    await fastify.listen(port)
+    await fastify.listen(port, '0.0.0.0')
   } catch (e) {
     console.log(e)
     fastify.log.error(e)
